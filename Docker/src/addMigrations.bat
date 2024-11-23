3c8ca3543c4cd7bfa02ff75803e15cb27e37d88a
ainur <ainur@AINURPC>
2024-11-23 15:17:56 +0300

    Залил проект для дз по NoSQL

--- Docker/src/addMigrations.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-SET MIGRATION_NAME=%1
-IF "%MIGRATION_NAME%"=="" (
-    echo "Migration name is required."
-    exit /b 1
-)
-dotnet ef migrations add %MIGRATION_NAME% --project "C:\Users\ainur\source\repos\HomeWorkOtusNew\src\EntityFrameWorkCore" --startup-project "C:\Users\ainur\source\repos\HomeWorkOtusNew\src\PromoCodeFactory.WebHost" --context "EfDbContext"
